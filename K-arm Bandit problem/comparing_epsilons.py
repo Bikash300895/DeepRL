b53a477577d2c4579b96795939e790f8c0f2c112
Shuvendu Bikash <bikash11roy@gmail.com>
2017-10-27 18:29:37 +0600

    running the experiment
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- K-arm Bandit problem/comparing_epsilons.py
+++ K-arm Bandit problem/comparing_epsilons.py
@@ -14,3 +14,22 @@ class Bandit:
         self.N += 1
         self.mean = (1 - 1.0/self.N)*self.mean + 1.0/self.N*x
         
+# lets run the experiment
+def run_experiment(m1, m2, m3, eps, N):
+    bandits = [Bandit(m1), Bandit(m2), Bandit(m3)]
+    
+    data = np.empty(N)
+    
+    for i in range(N):
+        # epsilon greedy
+        p = np.random.random()
+        if p < eps:
+            j = np.random.choice(3)     # since we have 3 bandit
+        else:
+            j = np.argmax([b.mean for b in bandits])
+        x = bandits[j].pull()
+        bandits[j].update(x)
+        
+        # for the plot
+        data[i] = x
+    cumulative_average = np.cumsum(data) / (np.arange(N) + 1)
