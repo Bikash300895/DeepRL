034dd1dc1d7d0c3d19cafc42f4f14a707a85cc36
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-22 02:53:21 +0600

    generating training data from random action

--- OpenAI Gym/Cartpole/CartPole_Tensorflow_ANN.py
+++ OpenAI Gym/Cartpole/CartPole_Tensorflow_ANN.py
@@ -1,3 +1,7 @@
+import random
+from collections import Counter
+
+import numpy as np
 import gym
 
 env = gym.make('CartPole-v0')
@@ -25,7 +29,57 @@ def some_random_games_first():
                 break
 
 
+# its purpose is to generate data by taking random action that will be use to train the nn model
 def initial_population():
+    # [obs, moves]
     training_data = []
+
+    # all scores
     scores = []
+
+    # scores that met our threshold
     accepted_score = []
+
+    for _ in range(initial_games):
+        env.reset()
+
+        score = 0
+        game_memory = []
+        prev_observation = []
+
+        for _ in range(goal_steps):
+            # choose random action (0, 1)
+            action = random.randrange(0,2)
+            # do it
+            observation, reward, done, info = env.step(action)
+
+            # let's store  which action, at which observation result in what reward to train the nn
+            if len(prev_observation) > 0:
+                game_memory.append([prev_observation, action])
+            prev_observation = observation  # update observation
+            score += reward
+            if done: break
+
+        if score>score_requirement:
+            accepted_score.append(score)
+            for data in game_memory:
+                if data[1] == 1:
+                    output = [0,1]
+                elif data[1] == 0:
+                    output = [1, 0]
+                training_data.append([data[0], output])
+
+        scores.append(score)
+
+    # save to a file
+    training_data_save = np.array(training_data)
+    np.save('saved.npy', training_data_save)
+
+    # some stats here, to further illustrate the neural network magic!
+    print('Average accepted score:', np.mean(accepted_score))
+    print('Median score for accepted scores:', np.median(accepted_score))
+    print(Counter(accepted_score))
+
+    return training_data
+
+# initial_population()
