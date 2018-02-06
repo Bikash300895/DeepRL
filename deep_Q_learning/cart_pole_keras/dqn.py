871f7bb0b33acdd23f0b8f88d5e9a03ae54d4228
Shuvendu Bikash <bikash11roy@gmail.com>
2018-02-07 02:50:29 +0600

    playing cartpole with dqn
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- deep_Q_learning/cart_pole_keras/dqn.py
+++ deep_Q_learning/cart_pole_keras/dqn.py
@@ -5,6 +5,8 @@ import gym
 from keras import layers, models
 from collections import deque
 
+EPISODES = 1000
+
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
@@ -38,7 +40,22 @@ class DQNAgent:
             return random.randrange(self.action_size)
         act_values = self.model.predict(state)
         return np.argmax(act_values[0])  # First dimension is the batch dim
-
+    
+    def reply(self, batch_size = 32):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            
+            target = reward
+            if not done:
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
+                
+            target_f = self.model.predict(state)  # it will the the output of the model, but we know one (action, reward) pair
+            target_f[0][action] = target  # 0 for batch dim, action is the known action. updating the return for that action
+            
+            self.model.fit(state, target_f, epochs = 1, verbose=0)
+        
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
 if __name__ == '__main__':
     env = gym.make('CartPole-v1')
@@ -46,4 +63,28 @@ if __name__ == '__main__':
     action_size = env.action_space.n  # 2
     agent = DQNAgent(state_size, action_size)
     
+    # helper parameters
+    done = False
+    batch_size = 32
     
+    for e in range(EPISODES):
+        state = env.reset()  # shape = (4,)
+        state = np.reshape(state, [1, state_size])  # shape (1, 4) == (batch_dim, state_size)
+        
+        for time in range(500):
+            # env.render()  # to render the cartpole
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+                
+            if done:
+                print("episode: {}/{}, score: {}, e: {:.2}".format(e, EPISODES, time, agent.epsilon))
+                break
+        
+        if len(agent.memory) > batch_size:
+            agent.reply(batch_size)
+            
+    agent.model.save_weights('CartPole-DQN.h5')
