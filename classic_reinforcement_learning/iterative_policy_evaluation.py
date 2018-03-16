5706e6ba4309565c87a16263b898f78f1f109b35
Bikash <bikash11roy@gmail.com>
2018-03-16 20:28:39 +0600

    iterative policy evaluation for a random policy

--- classic_reinforcement_learning/iterative_policy_evaluation.py
+++ classic_reinforcement_learning/iterative_policy_evaluation.py
@@ -14,6 +14,27 @@ from grid_world import standard_grid
 import matplotlib.pyplot as plt
 
 
+def print_values(V, g):
+    for i in range(g.width):
+        print("---------------------------")
+        for j in range(g.height):
+            v = V.get((i, j), 0)
+            if v >= 0:
+                print(" %.2f|" % v, end="")
+            else:
+                print("%.2f|" % v, end="")  # -ve sign takes up an extra space
+        print("")
+
+
+def print_policy(P, g):
+    for i in range(g.width):
+        print("---------------------------")
+        for j in range(g.height):
+            a = P.get((i, j), ' ')
+            print("  %s  |" % a, end="")
+        print("")
+
+
 if __name__ == '__main__':
     # Initialize local variables
     grid = standard_grid()
@@ -25,4 +46,23 @@ if __name__ == '__main__':
     V = {}
     for s in states:
         V[s] = 0
+
+    while True:
+        biggest_change = 0
+        for s in grid.actions:
+            v = V[s]
+            new_v = 0
+            prob = 1 / (len(grid.actions[s]))
+            for a in grid.actions[s]:
+                grid.set_state(s)
+                r = grid.move(a)
+                new_v += prob * (r + gamma * V[grid.current_state()])
+            V[s] = new_v
+            biggest_change = max(biggest_change, np.abs(v - V[s]))
+
+        if biggest_change < small_enough:
+            break
+
+    print("Values for uniformly random actions: ")
+    print_values(V, grid)
 
